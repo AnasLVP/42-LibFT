b5287136de73ec39b5590e75d0547864024834cd
Anas Abouyazid <aabouyaz@bess-f2r2s16.clusters.42paris.fr>
2025-04-26 14:09:54 +0200

    put terminal functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aabouyaz <aabouyaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 19:15:05 by aabouyaz          #+#    #+#             */
-/*   Updated: 2025/04/26 13:32:24 by aabouyaz         ###   ########.fr       */
+/*   Updated: 2025/04/26 13:41:00 by aabouyaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 int				ft_atoi(const char *nptr);
 void			ft_bzero(void *s, unsigned long n);
@@ -47,5 +48,9 @@ char			**ft_split(char const *s, char c);
 char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+void			ft_putstr_fd(char *s, int fd);
 
 #endif
