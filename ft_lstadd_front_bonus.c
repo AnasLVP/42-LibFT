1de722c16549c67c5cd0d671b2ac6e4ca62379f6
Anas Abouyazid <aabouyaz@student.42paris.fr>
2025-05-28 19:36:34 +0200

    protection lstadd_front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aabouyaz <aabouyaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 17:34:05 by aabouyaz          #+#    #+#             */
-/*   Updated: 2025/05/28 16:22:58 by aabouyaz         ###   ########.fr       */
+/*   Updated: 2025/05/28 19:05:07 by aabouyaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
-	(*lst)->previous = new;
+	if (*lst)
+		(*lst)->previous = new;
 	*lst = new;
 }
